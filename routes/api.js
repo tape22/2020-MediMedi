d0dba8c6e327863741bf30f583a5ab812b979e66
Ubuntu <ubuntu@ip-172-31-37-107.ap-northeast-2.compute.internal>
2020-06-28 06:15:22 +0000

    클라한테 API 결과 값 전달,통신 성공

--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,7 @@ var json = require('xml-js');
 var { sc, au, rm } = require('../modules/utils');
 var url = 'http://apis.data.go.kr/1471057/MdcinPrductPrmisnInfoService/getMdcinPrductItem';
 var queryParams = '?' + encodeURIComponent('ServiceKey') + '=VD6O56pfN7UxrkSMBnnUS0stE6c3vvZiClYmIUGuO0LS37jUVukST9GU3cva9Ens5cx5eldbQ8qWqp7EbN7Ing%3D%3D';
-
+var result = '';
 // API에 검색하기
 router.post('/', ps, async (req, res) => {
   try {
@@ -40,27 +40,10 @@ router.post('/', ps, async (req, res) => {
           if (error) {
             res.status(sc.BAD_REQUEST).send(au.successFalse);
             console.log('err');
-          }
-          var result = json.xml2json(body, { compact: true });
-          res.status(sc.OK).send(au.successTrue(result));
-          var jsp = JSON.parse(result).response.body;
-          var item = jsp.items.item;
-
-          if (jsp.totalCount == 0) {
-            res.status(sc.API_NULL).send(au.successFalse(rm.API_NULL));
-          }
-
-          // 이름. 회사명, 일반/전문의약품 구분, 용기와 유통기한
-          console.log('이름,회사명,일반/전문의약품 구분', item[0].ITEM_NAME._text, item[0].ENTP_NAME._text, item[0].ETC_OTC_CODE._text, '\n');
-          console.log('저장방법,유효기간', item[1].STORAGE_METHOD._text, item[1].VALID_TERM._text, '\n');
-          // 효능효과 파싱해오기
-          console.log('효능효과:', item[1].EE_DOC_DATA.DOC.SECTION.ARTICLE.PARAGRAPH._cdata);
-          var NB_DOC_DATA = item[1].NB_DOC_DATA.DOC.SECTION.ARTICLE;
-          for (i = 0; i < NB_DOC_DATA.length; i++) {
-            console.log(NB_DOC_DATA[i]._attributes, '\n');
-            console.log(NB_DOC_DATA[i].PARAGRAPH,'\n');
-          }
         }
+		result = body;
+		console.log(result);
+	}
       );
     }
   } catch (err) {
@@ -71,12 +54,13 @@ router.post('/', ps, async (req, res) => {
 });
 
 // API 검색 결과 가져오기
-/*router.get('/', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    res.send(result);
+	  res.send(result);
+	  console.log('통신성공');
   } catch (err) {
     console.log('err');
   }
-});*/
+});
 
 module.exports = router;
