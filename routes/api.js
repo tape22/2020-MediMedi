68598c11edb91834fb69b772e2bb1ba6ce628dce
Ubuntu <ubuntu@ip-172-31-37-107.ap-northeast-2.compute.internal>
2020-08-25 00:57:13 +0000

    충돌해결

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const router = express.Router();
 var ps = require('../modules/python');
+//var search = require('../modules/search');
 var request = require('request');
 var cheerio = require('cheerio');
 var { sc, au, rm } = require('../modules/utils');
 var url = 'http://apis.data.go.kr/1471057/MdcinPrductPrmisnInfoService/getMdcinPrductItem';
 var iv = require('iconv-lite');
 var result = [];
+var result2 = [];
+var namelist = [];
 
-var namelist = ['NAME', 'ENTP', 'ETC', 'STORAGE', 'VALID', 'EE', 'UD', 'NB'];
-var aJson = new Object();
 // API에 검색하기
 router.post('/', ps, async (req, res) => {
   var medInfo = req.body;
   result.length = 0;
   medInfo = medInfo.toString();
+  console.log('첫 medinfo:', medInfo);
 
   if (medInfo == '') {
     console.log('값 못읽어옴.');
     res.status(sc.BAD_REQUEST).send(au.fail(sc.BAD_REQUEST, rm.NULL_VALUE));
   }
-
-  /* 추가적인 알고리즘 */
 
   /* API에 검색하기-> 나중에 모듈로 빼기  */
   if (medInfo.length != 0) {
@@ -45,50 +45,51 @@ router.post('/', ps, async (req, res) => {
       }
       var content = Buffer.from(body);
       var xmlContent = iv.decode(content, 'utf-8').toString();
+
       var $ = cheerio.load(xmlContent, { xmlMode: true });
+      var cnt = $('totalCount').text();
 
       /*totalCount가 0이면 에러처리하기  */
-      if ($('totalCount') == 0) {
+      if (cnt == 0) {
         res.status(sc.BAD_REQUEST).send(au.successFalse(rm.API_NULL));
       }
+
+      for (var j = 0; j < cnt; j++) {
+        namelist.push('NAME', 'ENTP', 'ETC', 'STORAGE', 'VALID', 'EE', 'UD', 'NB');
+      }
+      // console.log(namelist);
       $('item').each(function () {
         NAME = $(this).children('ITEM_NAME').text();
+        /* 어떤 게 맞는 것인지 묻기 */
+        // result.push(NAME);
         ENTP = $(this).children('ENTP_NAME').text();
+        // result.push(ENTP);
         ETC = $(this).children('ETC_OTC_CODE').text();
+        // result.push(ETC);
         STORAGE = $(this).children('STORAGE_METHOD').text();
+        // result.push(STORAGE);
         VALID = $(this).children('VALID_TERM').text();
+        // result.push(VALID);
         EE = $(this).children('EE_DOC_DATA').text();
         UD = $(this).children('UD_DOC_DATA').text();
         NB = $(this).children('NB_DOC_DATA').text();
 
         result.push(NAME, ENTP, ETC, STORAGE, VALID, EE, UD, NB);
-
-        for (var i in result) {
-          result[i] = result[i].replace(/\r/g, '');
-          result[i] = result[i].replace(/\n/g, '');
-          aJson[namelist[i]] = result[i];
+        for (var j = 0; j < cnt; j++) {
+          var aJson = new Object();
+          for (var i = 0; i < result.length; i++) {
+            result[i] = result[i].replace(/\r/g, '');
+            result[i] = result[i].replace(/\n/g, '');
+            aJson[namelist[i]] = result[i];
+          }
+          result2.push(aJson);
         }
-        // aJson.name = NAME;
-        // aJson.entp = ENTP;
-        // aJson.etc = ETC;
-        // aJson.storage = STORAGE;
-        // aJson.valid = VALID;
-
-        // EE = EE.replace(/\n/g, ''); // 개행문자 제거
-        // EE = EE.replace(/\r/g, '');
-        // UD = UD.replace(/\n/g, ''); // 개행문자 제거
-        // UD = UD.replace(/\r/g, '');
-        // NB = NB.replace(/\n/g, ''); // 개행문자 제거
-        // NB = NB.replace(/\r/g, '');
-        // aJson.ee = EE;
-        // aJson.ud = UD;
-        // aJson.nb = NB;
       });
-      var sJson = JSON.stringify(aJson);
-      console.log(aJson);
 
-      res.status(sc.OK).send(au.successTrue(aJson));
+      result2 = Array.from(new Set(result2.map(JSON.stringify))).map(JSON.parse);
 
+      //var sJson = JSON.stringify(aJson);
+      res.status(sc.OK).send(au.successTrue(result2));
       return;
     });
   } catch (err) {
@@ -101,12 +102,20 @@ router.post('/', ps, async (req, res) => {
 // API 검색 결과 가져오기
 router.get('/', async (req, res) => {
   try {
-    res.send(aJson);
-    result.length = 0;
-    console.log('통신성공');
+    // 결과 값이 있으면 보내는 거 예외처리
+    if (Object.keys(aJson).length > 0) {
+      res.send(aJson);
+      result.length = 0;
+      console.log('결과 값 가져오기 성공');
+    } else {
+      res.status(sc.BAD_REQUEST).send(au.successFalse(rm.NULL_VALUE));
+      console.log('결과 값이 안읽힌거임');
+    }
   } catch (err) {
+    res.status(sc.BAD_REQUEST).send(au.successFalse(rm.NULL_VALUE));
     console.log('err');
   }
 });
 
 module.exports = router;
+
